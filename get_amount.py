8b05a21d5b479e920349572cb05d3757958770f3
Joe Wang <joewwwang1126@gmail.com>
2018-12-06 13:55:31 +0800

    Update get_amount.py
    Send word 'fee' to me on Wechat to get current fee.

--- get_amount.py
+++ get_amount.py
@@ -6,17 +6,22 @@ def main_():
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="autof460",
-    database="pachong"
+    passwd="xxx",
+    database="xxx"
     )
 
     mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM jilu")
+    mycursor.execute("SELECT * FROM xxx")
     myresult = mycursor.fetchall()
 
     for x in myresult:
         print(x[0])
     return x[0]
+#Below part is register the msg.
+"""Which means that. 
+When I get a msg 'fee'.
+I will reply the 'fee' I searched in my Sql.
+"""
 @itchat.msg_register(TEXT)
 def text_reply(msg):
     if msg.text=='fee':
@@ -31,9 +36,6 @@ def text_reply(msg):
 
 main_()
 a=main_()
-#print(time.time())
-#print(str(a))
 itchat.auto_login(enableCmdQR=2,hotReload=True)
-
 itchat.run()
 
